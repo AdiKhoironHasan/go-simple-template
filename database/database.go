24c34bc7c2a3099051688e5064d06048af5cc5c7
adi khoiron hasan <adi.khoiron@privyid.tech>
2023-06-10 19:17:07 +0700

    add postres

--- database/database.go
+++ database/database.go
@@ -6,11 +6,13 @@ import (
 	"go-simple-template/config"
 
 	"gorm.io/driver/mysql"
+	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 const (
-	driverMySQL = "mysql"
+	driverMySQL      = "mysql"
+	driverPostgreSQL = "postgres"
 )
 
 func NewConnection(cfg *config.Config) (*gorm.DB, error) {
@@ -31,6 +33,22 @@ func NewConnection(cfg *config.Config) (*gorm.DB, error) {
 		)
 
 		dbConn, err := gorm.Open(mysql.Open(dsn), gormConfig)
+		if err != nil {
+			return nil, err
+		}
+
+		return dbConn, nil
+
+	case driverPostgreSQL:
+		dsn = fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+			cfg.DBconfig.DBhost,
+			cfg.DBconfig.DBuser,
+			cfg.DBconfig.DBpassword,
+			cfg.DBconfig.DBname,
+			cfg.DBconfig.DBport,
+		)
+
+		dbConn, err := gorm.Open(postgres.Open(dsn), gormConfig)
 		if err != nil {
 			return nil, err
 		}
